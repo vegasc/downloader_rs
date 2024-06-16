fd232cd6997262a0d9bfb69c66832af1b67629ef
Aleksey Robul <alekseyrobul@gmail.com>
2024-06-16 13:06:37 +0200

    download progress log in progress

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,34 +1,43 @@
 use bytes::Bytes;
-use reqwest::Client;
+use futures::FutureExt;
+use futures_util::StreamExt;
 
 pub struct Downloader {
-    url: String,
-    pub(self) client: Client
+    url: String
 }
+
+// icon sample
+// https://cdn-icons-png.flaticon.com/256/1160/1160358.png
+
+
 
 impl Downloader {
 
     pub fn new(url: String) -> Downloader {
-        return Downloader { url:url, client: Client::new() };
+        return Downloader { url:url };
     }
-
+    
     pub async fn download(&self) -> Result<Box<Bytes>, Box<dyn std::error::Error>> {
-        let response = self.client
-        .get(self.url.to_string())
-        .send()
-        .await;
+        let stream = reqwest::get(&self.url).await?;
 
-        match response {
-            Ok(value) => {
-                let bytes: Bytes = value.bytes().await.unwrap();
-                let bytes_box: Box<Bytes> = Box::new(bytes);
-                return Ok(bytes_box);
-            }
-            Err(error) => {
-                return Err(Box::new(error));
-            }
+        let total_size = stream.content_length()
+            .ok_or("Failed to get content length")?;
+
+        let mut downloaded: Vec<u8> = Vec::new();
+        let mut downloaded_size = 0;
+
+        let mut stream_pin = Box::pin(stream.bytes().into_stream());
+
+        while let Some(item) = stream_pin.next().await {
+            let chunk = item?;
+            downloaded_size += chunk.len();
+            downloaded.extend_from_slice(&chunk);
+
+            let progress = downloaded_size as f64 / total_size as f64 * 100.0;
+            println!("Download progress: {:.2}%", progress);
         }
-        
+
+        Ok(Box::new(Bytes::from(downloaded)))
     }
 
 }
