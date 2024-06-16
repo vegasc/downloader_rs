9852cb56dbc40e070504c626e561c081174a6d17
Aleksey Robul <alekseyrobul@gmail.com>
2024-06-16 13:12:00 +0200

    download progress added

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,35 +1,28 @@
 use bytes::Bytes;
-use futures::FutureExt;
 use futures_util::StreamExt;
 
 pub struct Downloader {
-    url: String
+    url: String,
 }
 
-// icon sample
-// https://cdn-icons-png.flaticon.com/256/1160/1160358.png
-
-
-
 impl Downloader {
-
     pub fn new(url: String) -> Downloader {
-        return Downloader { url:url };
+        Downloader { url }
     }
-    
-    pub async fn download(&self) -> Result<Box<Bytes>, Box<dyn std::error::Error>> {
-        let stream = reqwest::get(&self.url).await?;
 
-        let total_size = stream.content_length()
+    pub async fn download(&self) -> Result<Box<Bytes>, Box<dyn std::error::Error>> {
+        let response = reqwest::get(&self.url).await?;
+
+        let total_size = response
+            .content_length()
             .ok_or("Failed to get content length")?;
 
+        let mut stream = response.bytes_stream();
         let mut downloaded: Vec<u8> = Vec::new();
         let mut downloaded_size = 0;
 
-        let mut stream_pin = Box::pin(stream.bytes().into_stream());
-
-        while let Some(item) = stream_pin.next().await {
-            let chunk = item?;
+        while let Some(chunk) = stream.next().await {
+            let chunk = chunk?;
             downloaded_size += chunk.len();
             downloaded.extend_from_slice(&chunk);
 
@@ -39,5 +32,4 @@ impl Downloader {
 
         Ok(Box::new(Bytes::from(downloaded)))
     }
-
 }
