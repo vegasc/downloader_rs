7d40167c42ea26372cf0307f69db90dfaffd51c3
Aleksey Robul <alekseyrobul@gmail.com>
2024-06-16 11:57:30 +0200

    downloader path fixes

--- src/file_namager.rs
+++ src/file_namager.rs
@@ -1,18 +1,29 @@
 use bytes::Bytes;
 use tokio::{fs::File, io::AsyncWriteExt};
 
+use std::path::PathBuf;
+
 pub struct FileManager {}
 
 impl FileManager {
 
-    pub async fn save_file<'a>(name: &str, path: &'a str, data:Box<Bytes>) -> Result<&'a str, Box<dyn std::error::Error>> {
-        let file = File::create(name).await;
+    pub async fn save_file<'a>(name: &str, path: &str, data:Box<Bytes>) -> Result<Box<String>, Box<dyn std::error::Error>> {
+        let mut file_path = PathBuf::from(path);
+        file_path.push(name);
+
+        let file_path_string = file_path
+                        .as_path()
+                        .to_str()
+                        .unwrap()
+                        .to_string();
+
+        let file = File::create(file_path).await;
         match file {
             Ok(mut value) => {
                 let write = value.write_all(&data).await;
                 match  write {
                     Ok(_) => {
-                        return Ok(path);
+                        return Ok(Box::new(file_path_string));
                     }
                     Err(error) => {
                         return Err(Box::new(error));
